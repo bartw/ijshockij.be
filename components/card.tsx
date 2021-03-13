02dbe5c169f3f15df99e5ad4bd86636050f8ea29
Bart Wijnants <bartwijnants@gmail.com>
2021-03-13 21:40:28 +0100

    fix responsiveness

--- components/card.tsx
+++ components/card.tsx
@@ -7,7 +7,7 @@ type Props = { children: ReactNode };
 export const Card = ({ children }: Props) => {
   const { width } = useCardContext();
 
-  const cardClassName = classNames("m-2 sm:m-0 sm:flex-shrink-0", {
+  const cardClassName = classNames("mt-2 sm:mt-0 sm:flex-shrink-0", {
     "sm:w-2/5 md:w-1/5": width === "narrow",
     "sm:w-3/5 md:w-2/5": width === "default",
   });
