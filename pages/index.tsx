6504446c8fd7de514f3dcaaed8cee0248ea525e5
Bart Wijnants <bartwijnants@gmail.com>
2021-03-13 10:01:04 +0100

    use next.js

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>ijshockij</title>
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    </Head>
+
+    <main>
+      <h1 className="text-2xl font-semibold">ijshockij</h1>
+    </main>
+  </div>
+);
+
+export default Home;
