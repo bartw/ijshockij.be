8640e08b18d437ed664c6036a18f966475ae545c
Bart Wijnants <bartwijnants@gmail.com>
2021-03-14 17:49:43 +0100

    use nocookie youtube domain

--- components/video.tsx
+++ components/video.tsx
@@ -4,7 +4,7 @@ export const Video = ({ id }: Props) => (
   <div className="aspect-w-16 aspect-h-9 sm:mr-8">
     <iframe
       title="video"
-      src={`https://www.youtube.com/embed/${id}`}
+      src={`https://www.youtube-nocookie.com/embed/${id}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
